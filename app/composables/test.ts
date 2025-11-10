a8a7f3db62688447b3427c3d6aaadf3f47ad796a
bright <bright@wisesolutions.co.kr>
2025-11-10 19:07:03 +0900

    init

---
+++ app/composables/test.ts
@@ -0,0 +1,201 @@
+const baseUrl = "https://developer-lostark.game.onstove.com";
+const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDA1OTEifQ.oZUpR_ZdnZFAM-O-gfrt52--ks9n7RG-Ey0kRnltTKo_skyzoAlFg1UBROUtWGJRdmqVmzFypLpecggLybjevxcxdPNZZOyumNNdnuRAMgnfOojkcJPbKejsiQ_LbSX8O0CdvQz9oyX49xlbq6ja5lh12z5tPu-HUj-o5_2rganpTw9l8ws30WMn3EpCuuDDRutqbthpYZ-0iUVPE6pry3oOK6BnO04qrP5fOy7v1nlslAknQscxAfTya7VxkPyMmASZdfVzLL2vAcZel1j2eyiTuqn4J6adgdfssAZCsP10oWhuNW7eiYvnsoTvtQGbSx1q40kZfQ8VbtEqCnUoqA";
+
+const get = async (path: string, params = {}) => {
+  try {
+    const response = await $fetch(baseUrl + path, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      query: params,
+    });
+    console.log(response);
+  } catch (error) {}
+};
+
+const post = async (path: string, params = {}) => {
+  try {
+    const response = await $fetch(baseUrl + path, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: params,
+    });
+    console.log(response);
+  } catch (error) {}
+};
+
+const news = {
+  async notices() {
+    try {
+      get(`/news/notices`, {});
+    } catch (error) {}
+  },
+  async events() {
+    try {
+      get(`/news/events`, {});
+    } catch (error) {}
+  },
+  async alarms() {
+    try {
+      get(`/news/alarms`, {});
+    } catch (error) {}
+  },
+};
+
+const characters = {
+  async siblings(characterName: String) {
+    try {
+      get(`/characters/${characterName}/siblings`, {});
+    } catch (error) {}
+  },
+};
+
+const armories = {
+  async profiles(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/profiles`, {});
+    } catch (error) {}
+  },
+  async equipment(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/equipment`, {});
+    } catch (error) {}
+  },
+  async avatars(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/avatars`, {});
+    } catch (error) {}
+  },
+  async combatSkills(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/combat-skills`, {});
+    } catch (error) {}
+  },
+  async engravings(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/engravings`, {});
+    } catch (error) {}
+  },
+  async cards(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/cards`, {});
+    } catch (error) {}
+  },
+  async gems(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/gems`, {});
+    } catch (error) {}
+  },
+  async colosseums(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/colosseums`, {});
+    } catch (error) {}
+  },
+  async collectibles(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/collectibles`, {});
+    } catch (error) {}
+  },
+  async arkpassive(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/arkpassive`, {});
+    } catch (error) {}
+  },
+  async arkgrid(characterName: String) {
+    try {
+      get(`/armories/characters/${characterName}/arkgrid`, {});
+    } catch (error) {}
+  },
+};
+
+const auctions = {
+  async options() {
+    try {
+      get(`/auctions/options`, {});
+    } catch (error) {}
+  },
+  async items() {
+    try {
+      let params = {
+        ItemLevelMin: 0,
+        ItemLevelMax: 0,
+        ItemGradeQuality: null,
+        ItemUpgradeLevel: null,
+        ItemTradeAllowCount: null,
+        SkillOptions: [
+          {
+            FirstOption: null,
+            SecondOption: null,
+            MinValue: null,
+            MaxValue: null,
+          },
+        ],
+        EtcOptions: [
+          {
+            FirstOption: null,
+            SecondOption: null,
+            MinValue: null,
+            MaxValue: null,
+          },
+        ],
+        Sort: "BIDSTART_PRICE",
+        CategoryCode: 0,
+        CharacterClass: "string",
+        ItemTier: null,
+        ItemGrade: "string",
+        ItemName: "string",
+        PageNo: 0,
+        SortCondition: "ASC",
+      };
+      post(`/auctions/items`, params);
+    } catch (error) {}
+  },
+};
+
+const markets = {
+  async options() {
+    try {
+      get(`/markets/options`, {});
+    } catch (error) {}
+  },
+  async itemId(itemId: String) {
+    try {
+      get(`/markets/items/${itemId}`, {});
+    } catch (error) {}
+  },
+  async items() {
+    try {
+      let params = {
+        Sort: "GRADE",
+        CategoryCode: 0,
+        CharacterClass: "string",
+        ItemTier: null,
+        ItemGrade: "string",
+        ItemName: "string",
+        PageNo: 0,
+        SortCondition: "ASC",
+      };
+      post(`/markets/items`, params);
+    } catch (error) {}
+  },
+};
+
+const gamecontents = {
+  async calendar() {
+    try {
+      get(`/gamecontents/calendar`, {});
+    } catch (error) {}
+  },
+};
+
+export default {
+  news,
+  characters,
+  armories,
+  auctions,
+  markets,
+  gamecontents,
+};
---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="test.news.notices()">공지</button>
+  <button @click="test.news.events()">이벤트</button>
+  <button @click="test.news.alarms()">알림</button>
+  <button @click="test.auctions.options()">경매장-옵션</button>
+  <button @click="test.auctions.items()">경매장-아이템</button>
+  <button @click="test.markets.options()">거래소-옵션</button>
+  <button @click="test.markets.itemId()">거래소-아이템아이디</button>
+  <button @click="test.markets.items()">거래소-아이템</button>
+  <button @click="test.gamecontents.calendar()">캘린더</button>
+
+  <button @click="test.characters.siblings('미르오스')">원정대</button>
+  <button @click="test.armories.profiles('미르오스')">프로필</button>
+  <button @click="test.armories.equipment('미르오스')">장비</button>
+  <button @click="test.armories.avatars('미르오스')">아바타</button>
+  <button @click="test.armories.combatSkills('미르오스')">스킬</button>
+  <button @click="test.armories.engravings('미르오스')">각인</button>
+  <button @click="test.armories.cards('미르오스')">카드</button>
+  <button @click="test.armories.gems('미르오스')">보석</button>
+  <button @click="test.armories.colosseums('미르오스')">증명의 전장</button>
+  <button @click="test.armories.collectibles('미르오스')">수집형 포인트</button>
+  <button @click="test.armories.arkpassive('미르오스')">아크 패시브</button>
+  <button @click="test.armories.arkgrid('미르오스')">아크 그리드</button>
+
+  <main>
+    
+  </main>
+</template>
+<script setup></script>
